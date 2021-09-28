8e5fdcb1a625b009bb28eff868dd2b070cbbd7c1
Igor Strelchenya <just.sicarius@gmail.com>
2021-09-28 17:34:55 +0300

    Create session
    1. Save sessions to database

--- src/main/java/com/example/sweater/config/WebSecurityConfig.java
+++ src/main/java/com/example/sweater/config/WebSecurityConfig.java
@@ -34,11 +34,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "/registration", "/static/**", "/activate/*").permitAll()
                 .anyRequest().authenticated()
-                .and()
+           .and()
                 .formLogin()
                 .loginPage("/login")
                 .permitAll()
-                .and()
+           .and()
+                .rememberMe()
+           .and()
                 .logout()
                 .permitAll();
     }
